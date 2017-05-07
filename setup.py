ec0b2e04b3acbbee5cf576e61a5e1d8457181b9f
Juan Morales <juan.morales@quby.com>
2017-05-07 19:37:07 +0200

    better setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='sas',
+setup(name='sash',
       version='0.1',
       description='Search multiple shell histories',
-      url='XXX',
+      url='https://github.com/wakaru44/sash',
       author='Wakaru44',
       author_email='wakaru44@gmail.com',
       license='MIT',
