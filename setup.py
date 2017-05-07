90a1b5027cdf127c902854db5fa02ac4c15d9b36
Juan Morales <juan.morales@quby.com>
2017-05-07 19:48:19 +0200

    taking steps towards a nice binary distribution

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README') as f:
+        return f.read()
+
 setup(name='sash',
       version='0.1',
       description='Search multiple shell histories',
@@ -7,5 +12,6 @@ setup(name='sash',
       author='Wakaru44',
       author_email='wakaru44@gmail.com',
       license='MIT',
+      scripts = ["bin/sas"],
       packages=['super_advanced_shell_search'],
       zip_safe=False)
